12a96da978067ba5775ba48ed3068f57a1401374
matheussASR <rm551466@fiap.com>
2024-03-14 12:29:26 -0300

    fix: alterando logo

--- secure_box/src/styles.js
+++ secure_box/src/styles.js
@@ -2,8 +2,6 @@ import { StyleSheet } from "react-native";
 
 export const colors = {
   primary: '#007bff',
-  secondary: '#6c757d',
-  background: '#f8f9fa',
 };
 
 export default StyleSheet.create({
