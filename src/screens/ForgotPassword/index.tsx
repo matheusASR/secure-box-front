4fe3ab4388d558c20be030ef80a62c8622f22c9a
matheussASR <rm551466@fiap.com>
2024-04-12 16:02:27 -0300

    feat: adicionando login alternativo

--- src/screens/ForgotPassword/index.tsx
+++ src/screens/ForgotPassword/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import {
   Text,
   TextInput,
@@ -12,9 +12,16 @@ import {
 } from "react-native";
 import styles from "./styles";
 import Toast from "react-native-root-toast";
+import { api } from "../../services/api";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { LoginContext } from "../../providers/loginContext";
 
 const ForgotPasswordScreen = () => {
   const [email, setEmail] = useState("");
+  const [code, setCode] = useState("");
+  const [codeLogin, setCodeLogin] = useState("");
+  const [emailSent, setEmailSent] = useState(false);
+  const { setLogged } = useContext(LoginContext)
 
   const generateToastConfig = (message: any) => {
     return [
@@ -30,32 +37,66 @@ const ForgotPasswordScreen = () => {
     ];
   };
 
-  const handleSendCode = () => {
-    // Aqui você precisa enviar uma solicitação para o seu backend para gerar e enviar um código para o email fornecido
-    // Esta é uma simulação de envio de código por email
-    const generateAndSendCode = async () => {
-      try {
-        // Lógica simulada para gerar um código de recuperação (número aleatório de 6 dígitos)
-        const recoveryCode = Math.floor(100000 + Math.random() * 900000);
+  function generateSixDigitPassword() {
+    let password = '';
+    for (let i = 0; i < 6; i++) {
+      const digit = Math.floor(Math.random() * 10); 
+      password += digit;
+    }
+    return password;
+  }
 
-        // Simulação de uma chamada de API para enviar o código por email
-        // Substitua este trecho com sua lógica real de backend para enviar o código por email
-        // Por exemplo, você pode usar uma API REST ou Firebase Cloud Functions
-        // Aqui, vamos apenas exibir um alerta com o código gerado
-        const [message, toastConfig] = generateToastConfig(`Código enviado!`);
-        Toast.show(message, toastConfig);
-
-        // Limpar o campo de email após o envio
-        setEmail("");
-      } catch (error: any) {
-        const [message, toastConfig] = generateToastConfig(`Erro ao enviar código: ${error.response.data.message}`);
+  const handleSendCode = async () => {
+    const code = generateSixDigitPassword()
+    setCode(code)
+    const payload = {
+      email: email.toLowerCase(),
+      code: code
+    }
+    try {
+      const response = await api.patch("/users", payload);
+      if (response.status === 200) {
+        setEmailSent(true)
+        const [message, toastConfig] = generateToastConfig(
+          "Código enviado com sucesso!"
+        );
         Toast.show(message, toastConfig);
       }
-    };
-
-    // Chamada da função de simulação de envio de código por email
-    generateAndSendCode();
+    } catch (error: any) {
+      const [message, toastConfig] = generateToastConfig(
+        `Ocorreu um erro ao enviar código de acesso: ${error.response.data.message}`
+      );
+      Toast.show(message, toastConfig);
+    }
   };
+
+  const loginAlt = async () => {
+    if (codeLogin === code) {
+      try {
+        const response = await api.post("/login/code", {
+          email: email.toLowerCase()
+        });
+        if (response.status === 200) {
+          await AsyncStorage.setItem("@secbox:TOKEN", response.data.token);
+          setLogged(true);
+          const [message, toastConfig] = generateToastConfig(
+            "Login realizado com sucesso! Você será redirecionado."
+          );
+          Toast.show(message, toastConfig);
+        }
+      } catch (error: any) {
+        const [message, toastConfig] = generateToastConfig(
+          `Ocorreu um erro ao fazer login: ${error.response.data.message}`
+        );
+        Toast.show(message, toastConfig);
+      }
+    } else {
+      const [message, toastConfig] = generateToastConfig(
+        `Código inválido!`
+      );
+      Toast.show(message, toastConfig);
+    }
+  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -65,21 +106,46 @@ const ForgotPasswordScreen = () => {
           style={styles.container}
         >
           <View style={styles.contentContainer}>
-            <Text style={styles.title}>Esqueceu a senha?</Text>
-            <Text style={styles.subtitle}>
-              Por favor, insira o email associado à sua conta:
-            </Text>
-            <TextInput
-              style={styles.input}
-              placeholder="Email*"
-              value={email}
-              onChangeText={setEmail}
-              keyboardType="email-address"
-              autoCapitalize="none"
-            />
-            <TouchableOpacity style={styles.button} onPress={handleSendCode}>
-              <Text style={styles.buttonText}>Enviar código</Text>
-            </TouchableOpacity>
+            {emailSent ? (
+              <>
+                <Text style={styles.subtitle}>
+                  Por favor, insira o código enviado por email:
+                </Text>
+                <TextInput
+                  style={styles.input}
+                  value={codeLogin}
+                  onChangeText={setCodeLogin}
+                  keyboardType="numeric"
+                />
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={() => loginAlt}
+                >
+                  <Text style={styles.buttonText}>Entrar</Text>
+                </TouchableOpacity>
+              </>
+            ) : (
+              <>
+                <Text style={styles.title}>Esqueceu a senha?</Text>
+                <Text style={styles.subtitle}>
+                  Por favor, insira o email associado à sua conta:
+                </Text>
+                <TextInput
+                  style={styles.input}
+                  placeholder="Email*"
+                  value={email}
+                  onChangeText={setEmail}
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                />
+                <TouchableOpacity
+                  style={styles.button}
+                  onPress={handleSendCode}
+                >
+                  <Text style={styles.buttonText}>Enviar código</Text>
+                </TouchableOpacity>
+              </>
+            )}
           </View>
         </KeyboardAvoidingView>
       </TouchableWithoutFeedback>
@@ -88,4 +154,3 @@ const ForgotPasswordScreen = () => {
 };
 
 export default ForgotPasswordScreen;
-
--- src/screens/ForgotPassword/styles.ts
+++ src/screens/ForgotPassword/styles.ts
@@ -45,6 +45,7 @@ const styles = StyleSheet.create<any>({
     backgroundColor: "white",
     width: "100%"
   },
+  
 });
 
 export default styles;
